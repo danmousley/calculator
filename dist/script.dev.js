ebca07cf6872aa4363e8e2735fd8e338f0beb4af
danmousley <dannymousley@outlook.com>
2021-08-12 15:46:32 +0700

    added quals button functionaility. Calculator now fully functioning

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -6,7 +6,7 @@ var operator = "";
 var result = 0.0;
 var strNumber = "";
 var display = "0";
-var subDisplay = "";
+var subDisplay = ""; //function to clear screen and all variables
 
 var clear = function clear() {
   previousNumber = undefined;
@@ -28,8 +28,39 @@ var updateScreen = function updateScreen() {
   document.querySelector(".display__calc").innerHTML = subdisplay;
 };
 
+var calculate = function calculate() {
+  if (operator == '+') {
+    result = +parseFloat(previousNumber + currentNumber).toFixed(8); //fixes decimal places to 8 maximum
+
+    display = result.toString();
+    previousNumber = result;
+    currentNumber = "";
+    operator = "";
+  } else if (operator == '-') {
+    result = +parseFloat(previousNumber - currentNumber).toFixed(8);
+    display = result.toString();
+    previousNumber = result;
+    currentNumber = "";
+    operator = "";
+  } else if (operator == '/') {
+    result = +parseFloat(previousNumber / currentNumber).toFixed(8);
+    display = result.toString();
+    previousNumber = result;
+    currentNumber = "";
+    operator = "";
+  } else {
+    result = +parseFloat(previousNumber * currentNumber).toFixed(8);
+    display = result.toString();
+    previousNumber = result;
+    currentNumber = "";
+    operator = "";
+  }
+
+  check();
+};
+
 document.addEventListener('DOMContentLoaded', function () {
-  // Access all number buttons
+  // access all number buttons
   var numbers = document.querySelectorAll(".button--number"); // when a number is clicked, update strNumber and display with number value
 
   numbers.forEach(function (number) {
@@ -37,6 +68,11 @@ document.addEventListener('DOMContentLoaded', function () {
       // check if equals was just pressed
       if (operator === "" && strNumber === "") {
         clear();
+      } // clear display
+
+
+      if (strNumber === "") {
+        display = "";
       }
 
       strNumber += event.target.value;
@@ -47,15 +83,85 @@ document.addEventListener('DOMContentLoaded', function () {
         display += event.target.value;
       }
 
-      subdisplay += event.target.value; // updateScreen()
+      subdisplay += event.target.value;
+      check();
+      updateScreen();
+    });
+  }); // access all operator buttons
+
+  var operators = document.querySelectorAll(".button--operator"); // when operator button clicked...
+
+  operators.forEach(function (button) {
+    button.addEventListener("click", function (event) {
+      // console.log(event.target.value)
+      //normal case
+      if (strNumber) {
+        //if this is the first number entered
+        if (previousNumber === undefined) {
+          previousNumber = parseFloat(strNumber);
+          strNumber = "";
+          display = "".concat(previousNumber);
+          operator = event.target.value;
+          subdisplay += operator;
+        } //any subsequent numbers enetered
+        else {
+            currentNumber = parseFloat(strNumber);
+            strNumber = "";
+            check();
+            calculate();
+            operator = event.target.value;
+            subdisplay += operator;
+          } // if equals has just been pressed
+
+      } else {
+        operator = event.target.value;
+        subdisplay += operator;
+      }
 
       check();
       updateScreen();
     });
-  });
+  }); //clear screen and all variables when AC clicked
+
   var ac = document.querySelector("#ac");
-  var del = document.querySelectorAll("#del");
   ac.addEventListener("click", function (event) {
     clear();
+  }); //remove last character from displays and strNumber variables
+
+  var del = document.querySelector("#del");
+  del.addEventListener("click", function (event) {
+    if (strNumber) {
+      if (strNumber.length === 1) {
+        strNumber = strNumber.slice(0, -1);
+        display = "0";
+        subdisplay = subdisplay.slice(0, -1);
+      } else {
+        strNumber = strNumber.slice(0, -1);
+        display = strNumber;
+        subdisplay = subdisplay.slice(0, -1);
+      }
+    }
+
+    updateScreen();
+  }); // add functionality to equals button
+
+  var equals = document.querySelector(".button--equals");
+  console.log(equals);
+  equals.addEventListener("click", function (event) {
+    // if a number has just been typed
+    if (strNumber) {
+      if (previousNumber === undefined) {
+        previousNumber = parseFloat(strNumber);
+        strNumber = "";
+        display = "".concat(previousNumber);
+      } else {
+        currentNumber = parseFloat(strNumber);
+        strNumber = "";
+        check();
+        calculate();
+      }
+
+      updateScreen();
+    }
   });
 });
--- script.js
+++ script.js
@@ -6,6 +6,7 @@ let strNumber = ""
 let display = "0"
 let subDisplay = ""
 
+//function to clear screen and all variables
 let clear = () => {
     previousNumber = undefined
     currentNumber = undefined
@@ -33,9 +34,38 @@ let updateScreen = () => {
     document.querySelector(".display__calc").innerHTML = subdisplay
 }
 
+let calculate = () => {
+    if (operator == '+') {
+        result = +parseFloat(previousNumber + currentNumber).toFixed(8) //fixes decimal places to 8 maximum
+        display = result.toString()
+        previousNumber = result
+        currentNumber = ""
+        operator=""
+    } else if (operator == '-') {
+        result = +parseFloat(previousNumber - currentNumber).toFixed(8)
+        display = result.toString()
+        previousNumber = result
+        currentNumber = ""
+        operator=""
+    } else if (operator == '/') {
+        result = +parseFloat(previousNumber / currentNumber).toFixed(8)
+        display = result.toString()
+        previousNumber = result
+        currentNumber = ""
+        operator=""
+    } else {
+        result = +parseFloat(previousNumber * currentNumber).toFixed(8)
+        display = result.toString()
+        previousNumber = result
+        currentNumber = ""
+        operator=""
+    }
+    check()
+}
+
 document.addEventListener('DOMContentLoaded', function() {
 
-    // Access all number buttons
+    // access all number buttons
     let numbers = document.querySelectorAll(".button--number")
 
     // when a number is clicked, update strNumber and display with number value
@@ -45,6 +75,10 @@ document.addEventListener('DOMContentLoaded', function() {
             if (operator === "" && strNumber === "") {
                 clear()
             }
+            // clear display
+            if (strNumber ==="") {
+                display = ""
+            }
             strNumber += event.target.value
             if (display.charAt(0) === "0") {
                 display = event.target.value
@@ -52,17 +86,89 @@ document.addEventListener('DOMContentLoaded', function() {
                 display += event.target.value
             }
             subdisplay += event.target.value
-            // updateScreen()
             check()
             updateScreen()
         })
     })
 
+    // access all operator buttons
+    let operators = document.querySelectorAll(".button--operator")
+
+    // when operator button clicked...
+    operators.forEach((button) => {
+        button.addEventListener("click", (event) => {
+            // console.log(event.target.value)
+            //normal case
+            if (strNumber) {
+                //if this is the first number entered
+                if (previousNumber === undefined) {
+                    previousNumber = parseFloat(strNumber)
+                    strNumber = ""
+                    display = `${previousNumber}`
+                    operator = event.target.value
+                    subdisplay += operator
+                }
+                //any subsequent numbers enetered
+                else {
+                    currentNumber = parseFloat(strNumber)
+                    strNumber = ""
+                    check()
+                    calculate()
+                    operator = event.target.value
+                    subdisplay += operator
+                }
+            // if equals has just been pressed
+            } else {
+                operator = event.target.value
+                subdisplay += operator
+            }
+            check()
+            updateScreen()
+        } )
+    })
+
+    //clear screen and all variables when AC clicked
     let ac = document.querySelector("#ac")
-    let del = document.querySelectorAll("#del")
-    
     ac.addEventListener("click", (event) => {
         clear()
     })
+
+    //remove last character from displays and strNumber variables
+    let del = document.querySelector("#del")
+    del.addEventListener("click", (event) => {
+        if (strNumber) {
+            if (strNumber.length === 1) {
+                strNumber = strNumber.slice(0, -1)
+                display = "0"
+                subdisplay = subdisplay.slice(0, -1)
+            } else {
+                strNumber = strNumber.slice(0, -1)
+                display = strNumber
+                subdisplay = subdisplay.slice(0, -1)
+            }
+        }
+        updateScreen()
+    })
+
+    // add functionality to equals button
+    let equals = document.querySelector(".button--equals")
+    console.log(equals)
+    equals.addEventListener("click", (event) => {
+        // if a number has just been typed
+        if (strNumber) {
+            if (previousNumber===undefined) {
+                previousNumber = parseFloat(strNumber)
+                strNumber = ""
+                display = `${previousNumber}`
+            } else {
+                currentNumber = parseFloat(strNumber)
+                strNumber = ""
+                check()
+                calculate()
+            }
+            updateScreen()
+        }
+    })
+
 })
 
